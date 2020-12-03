557832943b5beb30a22d3c1fbdf876a39539506b
wesley587 <72465364+wesley587@users.noreply.github.com>
2020-12-03 17:36:12 -0300

    Update filter2.0.py

--- githubFunc/filter2.0.py
+++ githubFunc/filter2.0.py
@@ -1,4 +1,5 @@
-import requests
+#before
+'''import requests
 url = 'https://raw.githubusercontent.com/BC-SECURITY/Empire/a58e0a5e608b1ebed5f81d8018105d020735db89/data/module_source' \
       '/situational_awareness/network/powerview.ps1'
 
@@ -17,3 +18,15 @@ for x in parser2[1:]:
                 if parser_func.find(')') == -1:
                     if len(parser_func) < 100:
                         func_names.append(parser_func)
+'''
+
+#after:
+
+from bs4 import BeautifulSoup
+import requests
+url = 'https://github.com/BC-SECURITY/Empire/blob/a58e0a5e608b1ebed5f81d8018105d020735db89/data/module_source/situational_awareness/network/powerview.ps1'
+resp = requests.get(url)
+soup = BeautifulSoup(resp.content, 'html.parser')
+a = soup.find_all('span', {'class': 'pl-en'})
+for x in a:
+    print(x.text)
